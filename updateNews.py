5beccaa2ef149fa22ded92f2e8bad44bfeb28ad1
Suguman <suguman@users.noreply.github.com>
2020-07-05 12:28:36 -0500

    Add files via upload

---
+++ updateNews.py
@@ -0,0 +1,104 @@
+
+#List of all news items. 
+#Formatted in (Date, News) both in string format
+newsList = [("July 3, 2020", "Extended abstract on my Dissertation has been accepted to appear at <a href=\"http://highlights-conference.org/\">Highlights in Logic, Games and Automata 2020.</a>"),
+    ("July 1, 2020", "<b>Big News!</b> Moved to <a href=\"https://www.cis.upenn.edu/\">CIS@UPenn</a>.<br> Excited to start as a Postdoctoral Researcher with <a href =\"https://www.cis.upenn.edu/~alur/\">Prof. Rajeev Alur</a>."),
+    ("June 22, 2020", "Submitted my <a href=\"Thesis/PhDThesis.pdf\">PhD. Thesis</a>. This marks the end of an epic era.<br>I am deeply grateful to all who were on this journey with me. Read my <a href=\"Thesis/ack.pdf\">acknowledgement here</a>."),
+    ("May 8, 2020", "Successfully defended my Ph.D. Dissertation \"Automata-Based Quantitative Reasoning\" under the most unusual circumstances -- from my dining table over a remote talk in a BYO Beverage/Food session!<br>My advisor Moshe Vardi said I did an \"outstanding job\"! Could I have asked for anything more?"),
+    ("April 24, 2020","PhD Dissertation defense <a href=\"https://events.rice.edu/#!view/event/event_id/90777\">official annoucement</a> is out today. The finish line is near!!!"),
+    ("March 14, 2020","Had been practicing social distancing for the past few weeks. Officially beginning strict Stay-at-home today. This is the beginning of the end of the world as we had come to know it. Hoping that we'll create a better world once we are out of the pandemic. Yup, that is my birthday wish for this year.  :)"),
+    ("Jan.-Feb. 2020", "Busy few days ahead. Pardon  me if I loose track of where I am. :P <br><ul> <li> January 14-15. Visiting U Penn. Hosted by <a href=\"https://www.cis.upenn.edu/~alur/\">Prof. Rajeev Alur</a>. Talking about \"Automata-based quantiative reasoning\" </li><li> January 22-24. Attending <a href=\"https://popl20.sigplan.org/\">POPL 2020</a>. I will present \"Synthesis of coordination programs from linear temporal logic\" (<a href=\"Papers/POPL20.pdf\">Paper</a>). This is joint work with Kedar S. Namjoshi and Yaniv Sa'ar.</li><li>February 3-5. Visiting UT Austin. Hosted by <a href=\"https://www.oden.utexas.edu/people/1430/\">Prof. Ufuk Topcu</a>. Talking about \"Designing intelligent systems via reactive synthesis\".</li><li>February 7. Visiting Nokia Bell Labs, Murray Hill. Always a pleasue to be back.</li><li>February 8-12. Attending <a href=\"https://aaai.org/Conferences/AAAI-20/\">AAAI 2020</a>. I will present \"Hybrid compositional reasoning for reactive synthesis from finite-horizon specifications\" (<a href=\"Papers/AAAI20.pdf\">Paper</a>). This is joint work with Yong Li, Lucas M. Tabajara, and Moshe Y. Vardi</li></ul>"),
+    ("November 25-29, 2019", "Had a great week in <a href=\"https://www.cs.ox.ac.uk/people/marta.kwiatkowska/\">Marta Kwiatkowska's</a> group at the University of Oxford. Thank you for the wonderful visit! <br>Talked about \"Automata-based quantiative verification\"."),
+            ("November 10, 2019", "Paper accepted at  <a href=\"https://aaai.org/Conferences/AAAI-20/\">AAAI 2020</a>! First paper at an AI venue. AI,  here I come! Our paper presents the a new approach for syntheis of reactive systems from temporal logic over finite traces. This is joint work with Yong Li, Lucas M. Tabajara and Moshe Y. Vardi."),
+            ("November 5, 2019","Awarded the <a href=\"https://www.acm.org/publications/policies/artifact-review-badging\">ACM Artifact Evaluated Badge - Functional</a> for our prototype tool for Coordination synthesis presented in our POPL 2020 paper."),
+            ("October 9, 2019", "Elated that our paper on \"Synthesis of coordination programs from linear temporal logic\" with Kedar S. Namjoshi and Yaniv Sa'ar has been conditionally accepted to <a href=\"https://popl20.sigplan.org/\">POPL 2020</a>."), ("August 20, 2019", "Awarded the <a href=\"https://csweb.rice.edu/news/antunes-bansal-named-future-faculty-fellows?fbclid=IwAR2ekluuomEIb5baFwBT8rRVDCV8YdHFzFGh4KzzGsivHQFuRud5MgWOf-s\">Rice School of Engineering Future Faculty Fellow</a>. Thank you, SoE!"),
+            ("July 13-18, 2019", "Returning to NYC to present my research at <a href=\"http://i-cav.org/2019/\">CAV 2019</a>:<br> <ul><li>Kedar is giving an invited talk on our work on Asynchronous synthesis at <a href=\"https://cs.nyu.edu/acsys/synt2019/\">SYNT 2019</a> on July 14. </li><li>I will present <a href=\"Papers/SYNT19.pdf\">Coordination synthesis</a> at <a href=\"https://cs.nyu.edu/acsys/synt2019/\">SYNT 2019</a> on July 14. This is joint work with Kedar S. Namjoshi and Yaniv Sa'ar.</li><li>I will present <a href=\"Papers/CAV19.pdf\">Safety and co-safety comparator automata</a> at <a href=\"http://i-cav.org/2019/\">CAV 2019</a> on July 15. This is joint work with Moshe Y. Vardi.</li></ul>See you at the talks!"),
+            ("June 3, 2019", "Paper titled \"Coodination synthesis\" accepted to <a href=\"https://cs.nyu.edu/acsys/synt2019/\">SYNT 2019</a>. This is joint work with Kedar S. Namjoshi and Yaniv Sa\'ar."),
+            ("April 25, 2019", "Visiting the Department of Computer Science - IIT Delhi today. Thank you <a href=\"http://www.cse.iitd.ernet.in/~sanjiva/\">Sanjiva Prasasd</a> for hosting me! <br> Talk title: Designing intelligent systems via reactive synthesis"),
+            ("April 16, 2019", "Paper titled \"Safety and co-safety comparators for discounted-sum inclusion\" has been accepted to <a href = \"http://i-cav.org/2019/\">CAV 2019</a>. This is joint work with Moshe Y. Vardi. <br>Can't wait to return to CAV!"),
+            ("April 15, 2019", "Visiting the School of Computing at NUS, Singapore today. Thank you <a href=\"https://www.comp.nus.edu.sg/~chinwn/\">Wei Ngan</a> for hosting me! <br> Talk title: Designing intelligent systems via reactive synthesis"),
+            ("March 25, 2019", "Journal version of \"Comparator automata for quantitative verification\" has been accepted for publication in <a href="">Logical Methods in Computer Science</a>. This is joint work with Swarat Chaudhuri and Moshe Y. Vardi."), ("October 28-30, 2018", "Attending the <a href=\"https://risingstars18-eecs.mit.edu/\">MIT EECS Rising Stars Workshop 2018</a>. It brings together the brightest and most inspiring rising women computer scientists for a phenomenal mentoring workshop. Honored to be in this cohort  of rising stars."),
+            ("July 14-17, 2018", "Attending <a href=\"http://cavconference.org/2018/\">CAV 2018</a> at <a href=\"http://www.floc2018.org/\">FLoC 2018</a> in Oxford, UK. I will be presenting my papers on <a href=\"Papers/CAV18a.pdf\">asyncrhonous syntheis</a> with Kedar S. Namjoshi and Yaniv Sa'ar on July 14, and <a href=\"Papers/CAV18b.pdf\">empirical evaluation of comparator-based and traditional approches for quantititative verification</a> with Swarat Chaudhuri and Moshe Y. Vardi on July 17. Drop by the talks!"),
+            ("June 4 - July 6, 2018","Back to Bell Labs for the summer."),
+            ("April 14-20, 2018", "Attending ETAPS 2018. Presenting paper on <a href=\"Papers/FoSSaCS18.pdf\">a symbolic technique for quantitative verification - Comparators</a> co-authored with Swarat Chaudhuri and Moshe Y. Vardi  at FoSSaCS 2018."),("April 11, 2018", "Priviledged to talk about my research at IST Austria."),
+            ("March 31, 2018", "What is better than one paper at CAV? Two papers at CAV!<br> Excited to share that two of my papers have been accepted for publication at CAV 2018.<br><ul><li>Work on in-practice performance of comparator-based algorithms vs traditional algorithms with Swarat Chaudhuri and Moshe Y. Vardi.</li><li>Work on asynchronous synthesis with Kedar S. Namjoshi and Yaniv Sa'ar.</li></ul>"),
+            ("March 25, 2018", "Spending the rest of the semester at <a href=\"https://simons.berkeley.edu/programs/realtime2018\">Simons Institute</a>, UC Berkeley. Already loving it. <3"),
+            ("March 7, 2018", "Who is the new Ph.D Candidate in town? Passed my qualifying exam!"),
+            ("December 22, 2017", "My work on Comparators with Swarat Chaudhuri and Moshe Vardi has been accepted for publication at FoSSaCS 2018. Great way to begin the winter holidays. :) "),
+            ("December 17-21, 2017","Wrapping up the year with a visit to IIAS, Jerusalem. Attending the <a href=\"http://ias.huji.ac.il/CSE2\">2nd Winter School on Computer Science and Engineering in Formal Verification</a>."),
+            ("September 19-21, 2017","Invited to attend and speak at the first ever Google Student Research Summit. My talk on \"Reasoning about Incentive Compatibility\" was well received."),
+            ("June-August, 2017", "Spending the summer interning at NOKIA-Bell Labs with Kedar Namjoshi. Was so much fun!"),("March 17, 2017", "Gave an impromptu talk on Comparators at Saarland University. Feeling researchy!"),
+            ("March 12 - 17, 2017", "My first ever Dagstuhl! Attending the <a href=\"http://www.dagstuhl.de/de/programm/kalender/semhp/?semnr=17111\">Dagsthul seminar on Games in AI, Logic and Algorithms</a>."),
+            ("December 15, 2016", "Selected to attend CRA-W Grad Cohort. Looking forward to it!"),
+            ("September 15, 2016","Officially a Master now. Submitted my <a href=\"MSThesis.pdf\">Master's thesis</a>."),("July 13, 2016", "Doesn't get better than the <a href=\"http://research.microsoft.com/en-us/events/fs2016/\">MSR Faculty Summit 2016</a>: Visiting MSR Redmond -- the Mecca for computer scientist, research showcase by the best researchers from all over the world, and inspiring talks by revolutionary leaders of CS."),("June 22, 2016","The big day has arrived! Headed home for my brother's wedding."), ("June 2, 2016","Summer travel begins! Visiting New York over the weekend."), ("April 27, 2016","Been invited to the <a href=\"http://research.microsoft.com/en-us/events/fs2016/\">MSR Faculty Summit 2016</a>. Feeling honored and excited!"), ("April 6, 2016", "Successfully defended my Masters!<br><u>Masters Thesis Title</u>: <b>Algorithmic analysis of Regular repeated games</b>"),("March 1, 2016", "Last week consisted of a paper submission to <a href=\"http://www.sigecom.org/ec16/\">EC 16</a>, three paper reviews, two course examinations, two course assignments, and one assignment grading <i>only</i>. And I thought post-submission will be free. :D"), ("January 24, 2016","Disney World! Dreams do come true. :)"),("January 23, 2016", "Acheivement unlocked. Gave my first talk outside Rice at <a href= \"http://conf.researchr.org/track/POPL-2016/POPL-2016-SRC\">Off the Beaten Track</a> at <a href=\"http://conf.researchr.org/home/POPL-2016\">POPL 2016</a>. Talk was on <i>Chanakya: Computer-Aided Strategic Reasoning</i>."),("January 22, 2016","My poster <i>Reasoning about Incentive Compatibility</i> has won the Gold Medal at the  <a href=\"http://conf.researchr.org/track/POPL-2016/POPL-2016-SRC\">ACM Student Research Competition</a> at <a href=\"http://conf.researchr.org/home/POPL-2016\">POPL 2016</a>. Yay!"), ("January 19, 2016","<a href=\"http://conf.researchr.org/home/POPL-2016\">POPL</a>ing for the rest of the week. I am on a mission this time. Presenting <br><ul><li>Poster at the <a href=\"http://conf.researchr.org/track/POPL-2016/POPL-2016-SRC\">Student Research Competition</a> on Jan 21</li><li>Talk at the  <a href = \"http://conf.researchr.org/track/POPL-2016/OBT-2016-talks#program\">Off the Beaten Track</a> on Jan 23</li></ul>"), ("December 29, 2015", "My poster titled <i>Reasoning about Incentive Compatibility</i> has been accepted to appear at <a href=\"http://conf.researchr.org/track/POPL-2016/POPL-2016-SRC\">Student Research Competition</a> at <a href=\"http://conf.researchr.org/home/POPL-2016\">POPL 2016</a>. See you at the competition!"),("December 2, 2015","Our paper titled <i>Chanakya: Computer-Aided Strategic Reasoning</i> has been accepted to appear at <a href=\"http://conf.researchr.org/home/OBT-2016\">Off the Beaten Track 2016</a> at <a href=\"http://conf.researchr.org/home/POPL-2016\">POPL 2016</a>. First paper acceptance!"), ("November 30, 2015","Submitted an extended abstract to <a href=\"http://conf.researchr.org/track/POPL-2016/POPL-2016-SRC\">Student Reseach Competition</a> at <a href=\"http://conf.researchr.org/home/POPL-2016\">POPL 2016</a>. Fingers crossed!"), ("November 17, 2015", "Submitted a paper to <a href=\"http://sis.smu.edu.sg/aamas2016?itemid=10096\">AAMAS 16.</a>"), ("November 12, 2015", "Saw a glimpse of grad school in a single day! Received a fellowship, did some research work, some TA duties, attended a great colloquium talk, met friends at the Diwali celebration, and a paper rejection. So many emotions!"), ("November 12, 2015", "First paper, first rejection. Such is research. :("), ("November 12, 2015", "Received the <a href=\"http://k2ifellowships.rice.edu/andrew-ladd-fellowship/\">Andrew Ladd Graduate Fellowship</a> from <a href=\"https://www.cs.rice.edu/about/\">Rice CS Department</a> and the <a href=\"http://www.k2i.rice.edu/\">Ken Kennedy Institute</a>. Such an honor! :)"),("November 9, 2015", "Submitted a paper to <a href = \"http://conf.researchr.org/home/OBT-2016\">OBT 16</a> at <a href=\"http://conf.researchr.org/home/POPL-2016\">POPL 16</a>."), ("October 28, 2015", "The affair of surprises and shocks with Rebuttals has begun. Last thing I expected reviews to be was amusing! :D"), ("October 19, 2015", "Gave my first research talk on \"Computer-Aided Strategic Reasoning\". Slides can be found <a href=\"https://www.dropbox.com/s/xtchk0n3otxxr75/Fall15.pdf?dl=0\">here</a>."),("October 14, 2015", "Updated <a href=\"index.html\">my webpage</a>. Was long overdue. :)"), ("September 15, 2015", "Submitted my first paper to <a href=\"http://www.aaai.org/Conferences/AAAI/aaai16.php\">AAAI 2016</a>!"), ("August 23, 2015","Back to Houston! Was a memorable summer. Enough fun, now let us get some work done!"), ("August 21, 2015","Last stop: Frankfurt."), ("August 18, 2015", "Vacation stop two: Meeting with my best friend for three days, after three years, in Europe's most beautiful city - Prague! Whatta reunion ;)"), ("August 16, 2015", "Vacation begins, finally! Exploring Munich, today and tomorrow."), ("August 3, 2015","Chapter Germany of Summer-15 begins! Spending the next two weeks in the Bavarian countryside participating in <a href = \"https://asimod.in.tum.de/\">Marktoberdorf Summer School</a>."), ("July 18, 2015","")]
+
+
+
+
+#Similar for index.html
+#newsBuffer differs in number of news items to write -- Hard coded
+#restBuffer differs -- Hard coded
+
+f1 = open("news.html", "r")
+
+l = f1.readline().lstrip().strip()
+
+textBuffer = ""
+newsBuffer = ""
+restBuffer = ""
+flag = 0
+while(flag == 0 ):
+    if l != "<h3>News</h3>":
+        textBuffer = textBuffer +  l + "\n"
+        l = f1.readline().lstrip().strip()
+        #print l
+    else:
+        newsBuffer = l + "\n<ul style=\"list-style-type:none\">\n<ul style=\"list-style-type:none\">\n"
+        
+        for (date, news) in newsList:
+            l = "<li><span class = \"timestamp\">{}</span>{}</li><br>\n".format(date, news)
+            newsBuffer += l
+        
+        restBuffer = "</ul>\n<p><a href=\"news.html\">All news&hellip;</a></p></div>\n<!--If you have any comments, questions, bug reports or suggestions regarding this template, or if you plan to customize this template into a new public theme for any CMS/Blog and want your version linked from my site, then you are very welcome to contact me through my website. <a href=\"#\">Read more...</a></p>\n<p class=\"credits\">&copy; 2012 Your Name<br />\nTemplate design by <a href=\"http://andreasviklund.com/\">Andreas Viklund</a> / Best hosted at <a href=\"https://www.svenskadomaner.se/?ref=mall&amp;ling=en\" title=\"Svenska Domaner AB\">www.svenskadomaner.se</a></p>\n	-->\n</div></div></body></html>\n"
+
+        flag = 1
+
+f1.close()
+
+f2 = open("news.html", "w")
+f2.write(textBuffer + newsBuffer + restBuffer)
+f2.close()
+
+
+# Similar for index.html
+# have hardcoded the number of news items to be displayed on the index page to 6.
+
+f1 = open("index.html", "r")
+
+l = f1.readline().lstrip().strip()
+
+textBuffer = ""
+newsBuffer = ""
+restBuffer = ""
+flag = 0
+while(flag == 0 ):
+    if l != "<h3>News</h3>":
+        textBuffer = textBuffer +  l + "\n"
+        l = f1.readline().lstrip().strip()
+        #print l
+    else:
+        newsBuffer = l + "\n<ul style=\"list-style-type:none\">\n<ul style=\"list-style-type:none\">\n"
+        
+        for (date, news) in newsList[:6]:
+            l = "<li><span class = \"timestamp\">{}</span>{}</li><br>\n".format(date, news)
+            newsBuffer += l
+        
+        restBuffer = "</ul>\n<p><a href=\"news.html\">All news&hellip;</a></p></div>\n<!--If you have any comments, questions, bug reports or suggestions regarding this template, or if you plan to customize this template into a new public theme for any CMS/Blog and want your version linked from my site, then you are very welcome to contact me through my website. <a href=\"#\">Read more...</a></p>\n<p class=\"credits\">&copy; 2012 Your Name<br />\nTemplate design by <a href=\"http://andreasviklund.com/\">Andreas Viklund</a> / Best hosted at <a href=\"https://www.svenskadomaner.se/?ref=mall&amp;ling=en\" title=\"Svenska Domaner AB\">www.svenskadomaner.se</a></p>\n	-->\n</div></div></body></html>\n"
+
+        flag = 1
+
+f1.close()
+
+f2 = open("index.html", "w")
+f2.write(textBuffer + newsBuffer + restBuffer)
+f2.close()
