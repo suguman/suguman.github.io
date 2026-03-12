fb9d7dfd47c5c16ff716bf137805a9badf20134b
suguman <suguman@users.noreply.github.com>
2026-03-12 20:38:32 +0000

    Deploying to gh-pages from @ suguman/suguman.github.io@c62fde7a3139da09c4806de417a4f82d3986ddef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
