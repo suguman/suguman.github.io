c9a63acd624ec4fe00d42222161a9b9e88b55424
suguman <suguman@users.noreply.github.com>
2026-03-13 19:54:21 +0000

    Deploying to gh-pages from @ suguman/suguman.github.io@f197b7d038b7dfeccff11e5dffab3f31ba27c803 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
